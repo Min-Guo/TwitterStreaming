2f9a10114095f0fc5e15a5dc46f320f9897207e0
Min Guo <Min@GuoMins-MacBook-Pro.local>
2016-08-02 21:38:49 -0700

    MultiTag works

--- src/main/scala/TweetsSentimentAnalyzer/CollectStreaming.scala
+++ src/main/scala/TweetsSentimentAnalyzer/CollectStreaming.scala
@@ -7,13 +7,14 @@ import org.apache.spark.{SparkConf, SparkContext}
 import twitter4j.auth.OAuthAuthorization
 import twitter4j.conf.ConfigurationBuilder
 import java.util.Properties
-
+import scala.collection.mutable.ArrayBuffer
 import TweetsSentimentAnalyzer.Sentiment._
 import edu.stanford.nlp.ling.CoreAnnotations
 import edu.stanford.nlp.neural.rnn.RNNCoreAnnotations
 import edu.stanford.nlp.pipeline.{Annotation, StanfordCoreNLP}
 import edu.stanford.nlp.sentiment.SentimentCoreAnnotations
 import org.joda.time.{DateTime, DateTimeZone}
+import twitter4j.{FilterQuery, HashtagEntity, Status}
 
 import scala.collection.convert.wrapAll._
 
@@ -48,28 +49,36 @@ object CollectStreaming{
       .toList
   }
 
-  //convert tweet string to Map type (key: hashTag, value: sentiment score)
-  def streamToMap(stream: String): Map[String, Any] = {
-    println("StartMapping!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-    val res = stream.split(",").map(_ split ":").map{ case Array(k, v) => (k, v) }.toMap
-    res.foreach{case (key, value) => println(key + "--->" + value)}
-    res
+   def mapHashTag(text: String): String = text match {
+     case text if text.contains("Java") => "Java"
+     case text if text.contains("Python") => "Python"
+     case text if text.contains("Go") => "Go"
+     case text if text.contains("Scala") => "Scala"
+     case text if text.contains("C++") => "C++"
+     case text if text.contains("Lisp") => "Lisp"
+     case text if text.contains("PHP") => "PHP"
+     case text if text.contains("JavaScript") => "JavaScript"
+     case text if text.contains("Ruby") => "Ruby"
+     case text if text.contains("Perl") => "Perl"
+   }
+
+
+  def multiTags(status: Status): ArrayBuffer[Map[String, Any]] = {
+    val length:Int = status.getHashtagEntities.length
+    val mapList = ArrayBuffer[Map[String, Any]]()
+    val index = 0
+    for (index <- 0 to (length - 1)) {
+      mapList += Map[String, Any]("hashTag" -> status.getHashtagEntities()(index).getText(), "score" -> mainSentiment(status.getText()), "timestamp" -> new DateTime(status.getCreatedAt).withZone(DateTimeZone.UTC))
+    }
+    mapList
   }
 
   def main(args: Array[String]) {
-//    val outputDir = new File("./temp")
-//    if (outputDir.exists()) {
-//      System.err.println("ERROR - %s already exists: delete or specify another directory")
-//      System.exit(1)
-//    }
-//    outputDir.mkdirs()
-
     println("Initializing Streaming Spark Context...")
     val conf = new SparkConf().setAppName(this.getClass.getSimpleName)
     val sc = new SparkContext(conf)
     val ssc = new StreamingContext(sc, Seconds(2))
-
-
+    val languageList = Array("Java", "Python", "Scala", "Go" , "C++", "Lisp", "PHP", "JavaScript", "Ruby", "Perl")
 
     //set twitter authority to access streaming data
     val tweetStream = TwitterUtils.createStream(ssc, Some(new OAuthAuthorization(new ConfigurationBuilder().setOAuthAccessToken("2523499370-jKz9tm4RWh96HcNs1G6kN5wMsUeuT3eJXSGoiAV")
@@ -77,30 +86,16 @@ object CollectStreaming{
       .setOAuthConsumerKey("dvUkoBr8N3kePgtaNXgFqIW2E")
       .setOAuthConsumerSecret("6Yix2c6gn5oGbOcdDsIgLkLy4EoJvjdArl2wcVnJT2hkdJBeA0").build())))
 
-
     //processing tweets streaming by spark job
     tweetStream.foreachRDD((rdd, time) => {
-      val count = rdd.count()
-      val hashTagRdd = rdd.filter(status => !status.getHashtagEntities().isEmpty)
-      //filter tweets from US.
-//      val enTweet = hashTagRdd.filter(_.getLang == "en")
-      val textTagPair = hashTagRdd.map(status => (status.getText(), status.getHashtagEntities()(0).getText()))
-      val tweetsSentimentPair = textTagPair.map(record => (record._2, mainSentiment(record._1)))
-//      val tweetString = hashTagRdd.map(status => "hashTag: " + status.getHashtagEntities()(0).getText() + ", " + "score: " + mainSentiment(status.getText()) + ", eventTimestamp: " + new DateTime(status.getCreatedAt))
-      val tweetsString = hashTagRdd.map(status => Map[String, Any]("hashTag" -> status.getHashtagEntities()(0).getText, "score" -> mainSentiment(status.getText()), "timestamp" -> new DateTime(status.getCreatedAt).withZone(DateTimeZone.UTC)))
+      //filter tweets with language.
+      val enTweet = rdd.filter(_.getLang == "en")
+      val tagTweets = enTweet.filter(status => !status.getHashtagEntities().isEmpty)
+      val tweetPair = tagTweets.map(status => (status.getHashtagEntities(), status))
+      val flatTags = enTweet.flatMap(status =>  multiTags(status))
       import com.metamx.tranquility.spark.BeamRDD._
       //pass data to druid
-      tweetsString.propagate(new MapBeamFactory)
-//      val tweetMap = tweetString.map(tweet => streamToMap(tweet)).propagate(new MapBeamFactory)
-       textTagPair.foreach(pair => println(pair._1 + "~~~~~~~~" + pair._2))
-      if (count > 0) {
-        val outputRDD = tweetsSentimentPair.repartition(5)
-//        outputRDD.saveAsTextFile("./temp" + "/tweets_" + time.milliseconds.toString)
-        numTweetsCollected += count
-        if (numTweetsCollected > args(0).toInt) {
-          System.exit(0)
-        }
-      }
+      flatTags.propagate(new MapBeamFactory)
     })
     ssc.start()
     ssc.awaitTermination()
